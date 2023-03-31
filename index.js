1aafd854205ea9ec2827347945059814bd71f82d
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-03-31 15:42:06 +0300

    ~push

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Your code here
+// cat.js
+class Cat {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  let fluffy = new Cat('Fluffy');
+  console.log(fluffy.speak()); // Output: "Fluffy says meow!"
+  
+  // dog.js
+  class Dog {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  module.exports = Dog;
+  // bird.js
+class Bird {
+    constructor(name, gender) {
+      this.name = name;
+      this.gender = gender;
+    }
+  
+    speak() {
+      if (this.gender === "male") {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+  
+  module.exports = Bird;
+  
+  // Your code here
